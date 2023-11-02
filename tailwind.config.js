5b0d3ecf6d234f23a94068bda71479a7653fcf68
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-03 01:20:18 +0400

    clear inital code and config tailwind.config for svelte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
